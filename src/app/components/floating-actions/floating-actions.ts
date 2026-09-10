4d3e9bf6b920744e1920baba8cb098ee12ab7bf0
eduardoflorenciano <eduardoflorencianoo@gmail.com>
2026-09-10 18:05:43 -0300

    refactor: refatora a arquitetura do projeto, removendo o 'pages'

---
+++ src/app/components/floating-actions/floating-actions.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-floating-actions',
+  styleUrl: './floating-actions.scss',
+  templateUrl: './floating-actions.html',
+})
+export class FloatingActions {}
---
+++ src/app/components/quantity-input/quantity-input.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-quantity-input',
+  styleUrl: './quantity-input.scss',
+  templateUrl: './quantity-input.html',
+})
+export class QuantityInput {}

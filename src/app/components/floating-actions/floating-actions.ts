35ec6a51c0039957dbcaa15dd04f24ac0ed070a7
eduardoflorenciano <eduardoflorencianoo@gmail.com>
2026-09-11 20:46:45 -0300

    feat: adiciona o componente do 'floating-actions'

--- src/app/components/floating-actions/floating-actions.ts
+++ src/app/components/floating-actions/floating-actions.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-floating-actions',
-  styleUrl: './floating-actions.scss',
+  imports: [],
   templateUrl: './floating-actions.html',
+  styleUrl: './floating-actions.scss',
 })
-export class FloatingActions {}
+export class FloatingActions {
+
+  private readonly numeroWhatsapp = '554599051549';
+  private readonly mensagemWhatsapp = 'Olá! Vim do site e gostaria de tirar uma dúvida sobre um pneu.';
+
+  protected readonly linkWhatsapp =
+    `https://wa.me/${this.numeroWhatsapp}?text=${encodeURIComponent(this.mensagemWhatsapp)}`;
+
+  
+  protected readonly mostrarBotaoTopo = signal(false);
+
+  @HostListener('window:scroll')
+  protected aoRolarPagina(): void {
+    this.mostrarBotaoTopo.set(window.scrollY > 300);
+  }
+
+  protected voltarAoTopo(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}

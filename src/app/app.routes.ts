820f70b4d3ef85103c0ff86e4641f2d033d5275b
eduardoflorenciano <eduardoflorencianoo@gmail.com>
2026-09-15 19:32:07 -0300

    feat: adiciona a rota do carrinho no 'app.routes.ts'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
+import { Carrinho } from './components/carrinho/carrinho';
 import { CatalogoBusca } from './components/catalogo-busca/catalogo-busca';
 import { Home } from './components/home/home';
 import { ProdutoDetalhe } from './components/produto-detalhe/produto-detalhe';
 
 export const routes: Routes = [
-  { path: '', component: Home }, 
+  { path: '', component: Home },
   { path: 'busca', component: CatalogoBusca },
   { path: 'produto/:id', component: ProdutoDetalhe },
+  { path: 'carrinho', component: Carrinho },
   { path: '**', redirectTo: '' },
 ];

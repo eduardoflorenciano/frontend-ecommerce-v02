6cdad92693ed543ee7a5969a4bb9883fef9802ee
vittor <vittor.moreno@escola.pr.gov.br>
2026-09-18 19:20:19 -0300

    feat: adiciona a rota children

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { Carrinho } from './components/carrinho/carrinho';
 import { CatalogoBusca } from './components/catalogo-busca/catalogo-busca';
 import { Home } from './components/home/home';
+import { Loginlist } from './components/login/loginlist/loginlist';
 import { ProdutoDetalhe } from './components/produto-detalhe/produto-detalhe';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'busca', component: CatalogoBusca },
-  { path: 'produto/:id', component: ProdutoDetalhe },
+  {
+    path: 'produto',
+    children: [{ path: ':id', component: ProdutoDetalhe }],
+  },
   { path: 'carrinho', component: Carrinho },
+  { path: 'login', component: Loginlist },
   { path: '**', redirectTo: '' },
 ];

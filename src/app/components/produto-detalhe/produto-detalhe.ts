34fca26304ddf991e903cb56a9c8a87f9d473886
vittor <vittor.moreno@escola.pr.gov.br>
2026-09-18 20:05:53 -0300

    refactor: refatora componente do produto detalhe

--- src/app/components/produto-detalhe/produto-detalhe.ts
+++ src/app/components/produto-detalhe/produto-detalhe.ts
@@ -1,14 +1,27 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { Router, RouterLink } from '@angular/router';
 import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
 import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { map, switchMap } from 'rxjs';
+import Swal from 'sweetalert2';
+import { ProdutoResumo } from '../../models/produto.model';
 import { CarrinhoService } from '../../services/carrinho.service';
 import { ProdutoService } from '../../services/produto.service';
 import { QuantityInput } from '../quantity-input/quantity-input';
 import { SecaoProdutos } from '../secao-produtos/secao-produtos';
 
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 2500,
+  timerProgressBar: true,
+});
+
 @Component({
+  standalone: true,
   selector: 'app-produto-detalhe',
   imports: [RouterLink, CurrencyPipe, MdbTabsModule, MdbRippleModule, SecaoProdutos, QuantityInput],
   templateUrl: './produto-detalhe.html',
@@ -19,20 +32,36 @@ export class ProdutoDetalhe {
   private readonly carrinhoService = inject(CarrinhoService);
   private readonly router = inject(Router);
 
-  protected readonly produto = this.produtoService.obterProdutoDetalhe();
+  readonly id = input.required<string>();
 
-  protected readonly produtosRelacionados = this.produtoService
-    .obterProdutosPromocao()
-    .filter((item) => item.id !== this.produto.id);
+  protected readonly produto = toSignal(
+    toObservable(this.id).pipe(switchMap((id) => this.produtoService.obterProdutoPorId(id))),
+  );
+
+  protected readonly outrosProdutos = toSignal(
+    this.produtoService
+      .listarTodos()
+      .pipe(map((produtos) => produtos.filter((item) => item.id !== this.id()))),
+    { initialValue: [] as ProdutoResumo[] },
+  );
 
   protected readonly quantidade = signal(1);
 
   protected adicionarAoCarrinho(): void {
-    this.carrinhoService.adicionarItem(this.produto, this.quantidade());
+    const produto = this.produto();
+    if (!produto) {
+      return;
+    }
+    this.carrinhoService.adicionarItem(produto, this.quantidade());
+    Toast.fire({ icon: 'success', title: `${produto.nome} adicionado ao carrinho!` });
   }
 
   protected comprarAgora(): void {
-    this.carrinhoService.adicionarItem(this.produto, this.quantidade());
+    const produto = this.produto();
+    if (!produto) {
+      return;
+    }
+    this.carrinhoService.adicionarItem(produto, this.quantidade());
     this.router.navigate(['/carrinho']);
   }
 }
